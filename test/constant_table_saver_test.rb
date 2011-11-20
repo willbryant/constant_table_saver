d17ebd8f0734ee3cbba9cec533495ae616421e84
Will Bryant <will.bryant@gmail.com>
2011-11-20 19:59:05 +1300

    extract out the hook code to support assert_queries

--- test/constant_table_saver_test.rb
+++ test/constant_table_saver_test.rb
@@ -1,4 +1,5 @@
 require File.expand_path(File.join(File.dirname(__FILE__), 'test_helper'))
+require File.expand_path(File.join(File.dirname(__FILE__), 'activerecord_count_queries'))
 
 class StandardPie < ActiveRecord::Base
   set_table_name "pies"
@@ -37,47 +38,6 @@ end
 class IngredientForConstantPie < ActiveRecord::Base
   set_table_name "ingredients"
   belongs_to :pie, :class_name => "ConstantPie"
-end
-
-if ActiveRecord::VERSION::MAJOR < 3 || (ActiveRecord::VERSION::MAJOR == 3 && ActiveRecord::VERSION::MINOR < 1)
-  # proudly stolen from ActiveRecord's test suite, with addition of BEGIN and COMMIT
-  ActiveRecord::Base.connection.class.class_eval do
-    IGNORED_SQL = [/^PRAGMA/, /^SELECT currval/, /^SELECT CAST/, /^SELECT @@IDENTITY/, /^SELECT @@ROWCOUNT/, /^SAVEPOINT/, /^ROLLBACK TO SAVEPOINT/, /^RELEASE SAVEPOINT/, /SHOW FIELDS/, /^BEGIN$/, /^COMMIT$/]
-
-    def execute_with_query_record(sql, name = nil, &block)
-      $queries_executed ||= []
-      $queries_executed << sql unless IGNORED_SQL.any? { |r| sql =~ r }
-      execute_without_query_record(sql, name, &block)
-    end
-
-    alias_method_chain :execute, :query_record
-  end
-else
-  # this is from 3.1's test suite.  ugly.
-  class ActiveRecord::SQLCounter
-    cattr_accessor :ignored_sql
-    self.ignored_sql = [/^PRAGMA (?!(table_info))/, /^SELECT currval/, /^SELECT CAST/, /^SELECT @@IDENTITY/, /^SELECT @@ROWCOUNT/, /^SAVEPOINT/, /^ROLLBACK TO SAVEPOINT/, /^RELEASE SAVEPOINT/, /^SHOW max_identifier_length/, /^BEGIN/, /^COMMIT/]
-
-    # FIXME: this needs to be refactored so specific database can add their own
-    # ignored SQL.  This ignored SQL is for Oracle.
-    ignored_sql.concat [/^select .*nextval/i, /^SAVEPOINT/, /^ROLLBACK TO/, /^\s*select .* from all_triggers/im]
-
-    def initialize
-      $queries_executed = []
-    end
-
-    def call(name, start, finish, message_id, values)
-      sql = values[:sql]
-
-      # FIXME: this seems bad. we should probably have a better way to indicate
-      # the query was cached
-      unless ['CACHE', 'SCHEMA'].include?(values[:name]) # we have altered this from the original, to exclude SCHEMA as well
-        # debugger if sql =~ /^PRAGMA table_info/ && Kernel.caller.any? {|i| i.include?('test_it_creates_named_class_methods_if_a_')}
-        $queries_executed << sql unless self.class.ignored_sql.any? { |r| sql =~ r }
-      end
-    end
-  end
-  ActiveSupport::Notifications.subscribe('sql.active_record', ActiveRecord::SQLCounter.new)
 end
 
 class ConstantTableSaverTest < ActiveRecord::TestCase
