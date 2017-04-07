38e54a190a3317924982cb8523a17c48aa43351b
Will Bryant <will.bryant@gmail.com>
2017-04-07 14:35:09 +1200

    remove rails 3 support, rails 4.0 coverage, and branch for rails 5 support

--- lib/constant_table_saver.rb
+++ lib/constant_table_saver.rb
@@ -8,10 +8,10 @@ module ConstantTableSaver
       class_attribute :constant_table_options, :instance_writer => false
       self.constant_table_options = options
       
-      if ActiveRecord::VERSION::MAJOR == 3
-        extend ActiveRecord3ClassMethods
-      else
+      if ActiveRecord::VERSION::MAJOR == 4
         extend ActiveRecord4ClassMethods
+      else
+        extend ActiveRecord5ClassMethods
       end
       extend ClassMethods
       extend NameClassMethods if constant_table_options[:name]
@@ -47,6 +47,64 @@ module ConstantTableSaver
       @constant_record_methods.each {|method_id| (class << self; self; end;).send(:remove_method, method_id)} if @constant_record_methods
       @cached_records = @cached_records_by_id = @constant_record_methods = @cached_blank_scope = @find_by_sql = nil
     end
+
+    def _to_sql_with_binds(sql, binds)
+      if sql.respond_to?(:to_sql)
+        # an arel object
+        connection.to_sql(sql, binds)
+      else
+        # a plain string
+        sql
+      end
+    end
+
+    def relation
+      super.tap do |s|
+        class << s
+          # we implement find_some here because we'd have to use partial string matching to catch
+          # this case in find_by_sql, which would be ugly.  (we do the other cases in find_by_sql
+          # because it's simpler & the only place to catch things like association find queries.)
+          def find_some(ids)
+            return super if @values.present? # special cases such as offset and limit
+            ids.collect {|id| find_one(id)}
+          end
+        end
+      end
+    end
+  end
+
+  module ActiveRecord5ClassMethods
+    def find_by_sql(sql, binds = [])
+      @find_by_sql ||= {
+        :all   => all.to_sql,
+        :id    => relation.where(relation.table[primary_key].eq(connection.respond_to?(:substitute_at) ? connection.substitute_at(columns_hash[primary_key], 0) : Arel::Nodes::BindParam.new)).limit(1).
+                    tap {|r| r.bind_values += [[columns_hash[primary_key], :undefined]]}. # work around AR 4.1.9-4.1.x (but not 4.2.x) calling nil.first if there's no bind_values
+                    arel,
+        :first => relation.order(relation.table[primary_key].asc).limit(1).to_sql,
+        :last  => relation.order(relation.table[primary_key].desc).limit(1).to_sql,
+      }
+
+      if binds.empty?
+        _sql = _to_sql_with_binds(sql, binds)
+
+        if _sql == @find_by_sql[:all]
+          return @cached_records ||= super(relation.to_sql).each(&:freeze)
+        elsif _sql == @find_by_sql[:first]
+          return [relation.to_a.first].compact
+        elsif _sql == @find_by_sql[:last]
+          return [relation.to_a.last].compact
+        end
+
+      elsif binds.length == 1 &&
+            binds.first.first.is_a?(ActiveRecord::ConnectionAdapters::Column) &&
+            binds.first.first.name == primary_key &&
+            _to_sql_with_binds(sql, binds) == _to_sql_with_binds(@find_by_sql[:id], binds) # we have to late-render the find(id) SQL because mysql2 on 4.1 and later requires the bind variables to render the SQL, and errors out with a nil dereference otherwise
+        @cached_records_by_id ||= relation.to_a.index_by {|record| record.id.to_param}
+        return [@cached_records_by_id[binds.first.last.to_param]].compact
+      end
+
+      super
+    end
   end
 
   module ActiveRecord4ClassMethods
@@ -80,121 +138,6 @@ module ConstantTableSaver
       end
 
       super
-    end
-
-    def _to_sql_with_binds(sql, binds)
-      if sql.respond_to?(:to_sql)
-        # an arel object
-        connection.to_sql(sql, binds)
-      else
-        # a plain string
-        sql
-      end
-    end
-
-    def relation
-      super.tap do |s|
-        class << s
-          # we implement find_some here because we'd have to use partial string matching to catch
-          # this case in find_by_sql, which would be ugly.  (we do the other cases in find_by_sql
-          # because it's simpler & the only place to catch things like association find queries.)
-          def find_some(ids)
-            return super if @values.present? # special cases such as offset and limit
-            ids.collect {|id| find_one(id)}
-          end
-        end
-      end
-    end
-  end
-
-  module ActiveRecord3ClassMethods
-    def scoped(options = nil)
-      return super if options
-      return super if respond_to?(:current_scoped_methods, true) && current_scoped_methods
-      return super if respond_to?(:current_scope, true) && current_scope
-      @cached_blank_scope ||= super.tap do |s|
-        class << s
-          def to_a
-            return @records if loaded?
-            super.each(&:freeze)
-          end
-          
-          def find(*args)
-            # annoyingly, the call to find to load a belongs_to passes :conditions => nil, which causes
-            # the base find method to apply_finder_options and construct an entire new scope, which is
-            # unnecessary and also means that it bypasses our find_one implementation (we don't interfere
-            # with scopes or finds that actually do apply conditions etc.), so we check as a special case
-            return find_with_ids(args.first) if args.length == 2 && args.last == {:conditions => nil}
-            super
-          end
-      
-          def find_first
-            # the normal scope implementation would cache this anyway, but we force a load of all records,
-            # since otherwise if the app used .first before using .all there'd be unnecessary queries
-            to_a.first
-          end
-        
-          def find_last
-            # as for find_first
-            to_a.last
-          end
-        
-          def find_one(id)
-            # see below re to_param
-            cached_records_by_id[id.to_param] || raise(::ActiveRecord::RecordNotFound, "Couldn't find #{name} with ID=#{id}")
-          end
-          
-          def find_some(ids)
-            # see below re to_param
-            ids.collect {|id| cached_records_by_id[id.to_param]}.tap do |results| # obviously since find_one caches efficiently, this isn't inefficient as it would be for real finds
-              results.compact!
-              raise(::ActiveRecord::RecordNotFound, "Couldn't find all #{name.pluralize} with IDs #{ids.join ','} (found #{results.size} results, but was looking for #{ids.size}") unless results.size == ids.size
-            end
-          end
-
-          # in Rails 3.1 the associations code was rewritten to generalise its sql generation to support
-          # more complex relationships (eg. nested :through associations).  however unfortunately, during
-          # this work the implementation of belongs_to associations was changed so that it no longer calls
-          # one of the basic find_ methods above; instead a vanilla target scope is constructed, a where()
-          # scope to add the constraint that the primary key = the FK value is constructed, the two are
-          # merged, and then #first is called on that scope.  frustratingly, all this complexity means that
-          # our find_ hooks above are no longer called when dereferencing a belongs_to association; they
-          # work fine and are used elsewhere, but we have to explicitly handle belongs_to target scope
-          # merging to avoid those querying, which is a huge PITA.  because we want to ensure that we don't
-          # end up accidentally caching other scope requests, we explicitly build a list of the possible
-          # ARel constrained scopes - indexing them by their expression in SQL so that we don't need to
-          # code in the list of all the possible ARel terms.  we then go one step further and make this
-          # cached scope pre-loaded using the record we already have - there's sadly no external way to do
-          # this so we have to shove in the instance variables.
-          #
-          # it will be clear that this was a very problematic ActiveRecord refactoring.
-          def belongs_to_record_scopes
-            @belongs_to_record_scopes ||= to_a.each_with_object({}) do |record, results|
-              scope_that_belongs_to_will_want = where(table[primary_key].eq(record.id))
-              scope_that_belongs_to_will_want.instance_variable_set("@loaded", true)
-              scope_that_belongs_to_will_want.instance_variable_set("@records", [record])
-              results[scope_that_belongs_to_will_want.to_sql] = scope_that_belongs_to_will_want
-            end.freeze
-          end
-
-          def merge(other)
-            if belongs_to_record_scope = belongs_to_record_scopes[other.to_sql]
-              return belongs_to_record_scope
-            end
-
-            super other
-          end
-        
-        private
-          def cached_records_by_id
-            # we'd like to use the same as ActiveRecord's finder_methods.rb, which uses:
-            #  id = id.id if ActiveRecord::Base === id
-            # but referencing ActiveRecord::Base here segfaults my ruby 1.8.7
-            # (2009-06-12 patchlevel 174) [universal-darwin10.0]!  instead we use to_param.
-            @cached_records_by_id ||= all.index_by {|record| record.id.to_param}
-          end
-        end
-      end
     end
   end
 
--- test/constant_table_saver_test.rb
+++ test/constant_table_saver_test.rb
@@ -54,8 +54,6 @@ class ConstantTableSaverTest < ActiveSupport::TestCase
     assert_queries(0, &block)
   end
 
-  DEPRECATED_FINDERS = ActiveRecord::VERSION::MAJOR == 3 || (ActiveRecord::VERSION::MAJOR == 4 && ActiveRecord::VERSION::MINOR == 0)
-
   test "it caches all() results" do
     @pies = StandardPie.all.to_a
     assert_queries(1) do
@@ -65,16 +63,6 @@ class ConstantTableSaverTest < ActiveSupport::TestCase
       assert_equal @pies.collect(&:attributes), ConstantPie.all.to_a.collect(&:attributes)
     end
   end
-
-  test "it caches find(:all) results" do
-    @pies = StandardPie.find(:all).to_a
-    assert_queries(1) do
-      assert_equal @pies.collect(&:attributes), ConstantPie.find(:all).to_a.collect(&:attributes)
-    end
-    assert_no_queries do
-      assert_equal @pies.collect(&:attributes), ConstantPie.find(:all).to_a.collect(&:attributes)
-    end
-  end if DEPRECATED_FINDERS
 
   test "it caches find(id) results" do
     @pie = StandardPie.find(1)
@@ -100,16 +88,6 @@ class ConstantTableSaverTest < ActiveSupport::TestCase
     end
   end
 
-  test "it caches find(:first) results" do
-    @pie = StandardPie.find(:first)
-    assert_queries(1) do
-      assert_equal @pie.attributes, ConstantPie.find(:first).attributes
-    end
-    assert_no_queries do
-      assert_equal @pie.attributes, ConstantPie.find(:first).attributes
-    end
-  end if DEPRECATED_FINDERS
-
   test "it caches first() results" do
     @pie = StandardPie.first
     assert_queries(1) do
@@ -119,16 +97,6 @@ class ConstantTableSaverTest < ActiveSupport::TestCase
       assert_equal @pie.attributes, ConstantPie.first.attributes
     end
   end
-
-  test "it caches find(:last) results" do
-    @pie = StandardPie.find(:last)
-    assert_queries(1) do
-      assert_equal @pie.attributes, ConstantPie.find(:last).attributes
-    end
-    assert_no_queries do
-      assert_equal @pie.attributes, ConstantPie.find(:last).attributes
-    end
-  end if DEPRECATED_FINDERS
   
   test "it caches last() results" do
     @pie = StandardPie.last
@@ -154,7 +122,6 @@ class ConstantTableSaverTest < ActiveSupport::TestCase
   
   test "it isn't affected by scopes active at the time of first load" do
     assert_equal 0, ConstantPie.filled_with_unicorn.all.to_a.size
-    assert_equal 0, ConstantPie.with_unicorn_filling_scope { ConstantPie.all.to_a.length } if DEPRECATED_FINDERS
     assert_equal StandardPie.all.to_a.size, ConstantPie.all.to_a.size
   end
   
@@ -193,28 +160,9 @@ class ConstantTableSaverTest < ActiveSupport::TestCase
     end
   end
   
-  test "it doesn't cache find queries with options" do
-    @pies = StandardPie.all(:select => "id").to_a
-    @pie = StandardPie.find(1, :select => "id")
-    assert_queries(3) do
-      assert_equal @pies.collect(&:attributes), ConstantPie.all(:select => "id").collect(&:attributes)
-      assert_equal @pies.collect(&:attributes), ConstantPie.find(:all, :select => "id").collect(&:attributes)
-      assert_equal @pie.attributes, ConstantPie.find(1, :select => "id").attributes
-    end
-    assert_queries(3) do
-      assert_equal @pies.collect(&:attributes), ConstantPie.all(:select => "id").collect(&:attributes)
-      assert_equal @pies.collect(&:attributes), ConstantPie.find(:all, :select => "id").collect(&:attributes)
-      assert_equal @pie.attributes, ConstantPie.find(1, :select => "id").attributes
-    end
-  end if DEPRECATED_FINDERS
-  
   test "it passes the options preventing caching to the underlying query methods" do
-    assert_equal nil, ConstantPie.where(:filling => 'unicorn').first
-    assert_equal nil, ConstantPie.first(:conditions => {:filling => 'unicorn'}) if DEPRECATED_FINDERS
-    assert_equal nil, ConstantPie.find(:first, :conditions => {:filling => 'unicorn'}) if DEPRECATED_FINDERS
+    assert_nil nil, ConstantPie.where(:filling => 'unicorn').first
     assert_equal [],  ConstantPie.where(:filling => 'unicorn').all
-    assert_equal [],  ConstantPie.all(:conditions => {:filling => 'unicorn'}) if DEPRECATED_FINDERS
-    assert_equal [],  ConstantPie.find(:all,   :conditions => {:filling => 'unicorn'}) if DEPRECATED_FINDERS
   end
   
   test "it creates named class methods if a :name option is given" do
