c72786be3d87c27e23e16a6e23b999cc7f5114d5
Will Bryant <will.bryant@gmail.com>
2017-05-31 22:40:08 +1200

    also tested against 5.0.3 as well as 5.0.2 and 5.1.1

--- test_all.rb
+++ test_all.rb
@@ -2,7 +2,7 @@
 
 require 'yaml'
 
-rails_versions = ["4.2.0".."4.2.3", "4.1.07".."4.1.12", "4.0.13", "3.2.18"].flat_map {|spec| Array(spec).collect {|v| v.gsub /.0(\d)/, '.\\1'}}
+rails_versions = ["5.1.1", "5.0.2".."5.0.3", "4.2.7"].flat_map {|spec| Array(spec).collect {|v| v.gsub /.0(\d)/, '.\\1'}}
 rails_envs = YAML.load(File.read("test/database.yml")).keys
 
 rails_envs.each do |env|
